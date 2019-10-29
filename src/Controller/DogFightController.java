5242b0b87d758621dc448dd9ca263aa01cca61f0
Scordragours <arthur.lecras@viacesi.fr>
2019-10-29 16:23:12 +0100

    Package controller : definition of the "DogFightController" class and the "IDogFightController" interface
    Signed-off-by: Scordragours <arthur.lecras@viacesi.fr>

--- src/Controller/DogFightController.java
+++ src/Controller/DogFightController.java
@@ -1,5 +1,41 @@
 package Controller;
 
-public class DogFightController{
+import View.IViewSystem;
 
+public class DogFightController implements IOrderPerformer{
+    // Atributs :
+    private static final int  TIME_SLEEP = 30;
+    private IViewSystem ViewSystem;
+
+    // Contructor :
+    public DogFightController(){
+
+    }
+
+    // Methodes :
+    public void OrderPerform(UserOrder UserOrder){
+
+    }
+    public void Play(){
+
+    }
+    private void LaunchMissile(int Player){
+
+    }
+    private void GameLoop(){
+
+    }
+
+    // Getter :
+    public static int getTimeSleep(){
+        return TIME_SLEEP;
+    }
+    public IViewSystem getViewSystem(){
+        return this.ViewSystem;
+    }
+
+    // Setter :
+    public void setViewSystem(IViewSystem ViewSystem){
+        this.ViewSystem = ViewSystem;
+    }
 }
--- src/Controller/IOrderPerformer.java
+++ src/Controller/IOrderPerformer.java
@@ -2,4 +2,6 @@ package Controller;
 
 public interface IOrderPerformer{
 
+    public void OrderPerform(UserOrder UserOrder);
+
 }
