bd6ad7dd0aafeb189571a4349436b7f70b4cc217
Scordragours <arthur.lecras@viacesi.fr>
2019-10-29 16:48:24 +0100

    Package Main : add Main
    Signed-off-by: Scordragours <arthur.lecras@viacesi.fr>

---
+++ src/Main/Main.java
@@ -0,0 +1,11 @@
+package Main;
+
+import Controller.DogFightController;
+
+public class Main{
+    public static void main(String args[]){
+        final DogFightController Controller = new DogFightController();
+        //Controller.setViewSystem(View);
+        Controller.Play();
+    }
+}
