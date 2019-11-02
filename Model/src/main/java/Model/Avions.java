f1fff058e39df69835d92a11194a2ddf9094dcfc
Scordragours <arthur.lecras@viacesi.fr>
2019-11-02 19:31:35 +0100

    - Definition of the Avion class.

--- Model/src/main/java/Model/Avions.java
+++ Model/src/main/java/Model/Avions.java
@@ -1,3 +1,69 @@
 package Model;
 
-class Avions{}
+class Avions extends Entity{
+    // Attributs :
+    private Equipe Equipe;
+    private int Score;
+    private State State;
+    private Direction Direction;
+    private long TempsReappear;
+    private String Pseudo;
+
+    // Constructeur :
+    public Avions(Equipe Equipe, String Pseudo){
+        this.setEquipe(Equipe);
+        this.setScore(0);
+        //this.setState();
+        //this.setDirection();
+        this.setTempsReappear(0);
+        this.setPseudo(Pseudo);
+    }
+    public Avions(){
+        //this.setEquipe();
+        this.setScore(0);
+        //this.setState();
+        //this.setDirection();
+        this.setTempsReappear(0);
+        this.setPseudo("none");
+    }
+
+    // Getter :
+    public Equipe getEquipe(){
+        return this.Equipe;
+    }
+    public int getScore(){
+        return this.Score;
+    }
+    public State getState(){
+        return this.State;
+    }
+    public Direction getDirection(){
+        return this.Direction;
+    }
+    public long getTempsReappear(){
+        return this.TempsReappear;
+    }
+    public String getPseudo(){
+        return this.Pseudo;
+    }
+
+    // Setter :
+    public void setEquipe(Equipe Equipe){
+        this.Equipe = Equipe;
+    }
+    public void setScore(int Score){
+        this.Score = Score;
+    }
+    public void setState(State State){
+        this.State = State;
+    }
+    public void setDirection(Direction Direction){
+        this.Direction = Direction;
+    }
+    public void setTempsReappear(long TempsReappear){
+        this.TempsReappear = TempsReappear;
+    }
+    public void setPseudo(String Pseudo){
+        this.Pseudo = Pseudo;
+    }
+}
