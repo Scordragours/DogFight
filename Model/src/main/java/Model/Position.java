5d4ab5dba54133722be1485b67b2e181312e7225
Scordragours <arthur.lecras@viacesi.fr>
2019-11-02 18:57:53 +0100

    - Definition of the Position class.

--- Model/src/main/java/Model/Position.java
+++ Model/src/main/java/Model/Position.java
@@ -1,3 +1,36 @@
 package Model;
 
-public class Position{}
+public class Position{
+    // Atributs
+    private double X, Y;
+
+    // Constructeur :
+    public Position(){
+        this.setX(0);
+        this.setY(0);
+    }
+    public Position(double[] Position){
+        this.setX(Position[0]);
+        this.setY(Position[1]);
+    }
+    public Position(double X, double Y){
+        this.setX(X);
+        this.setY(Y);
+    }
+
+    // Getter :
+    public double getX(){
+        return this.X;
+    }
+    public double getY(){
+        return this.Y;
+    }
+
+    // Setter :
+    public void setX(double X){
+        this.X = X;
+    }
+    public void setY(double Y){
+        this.Y = Y;
+    }
+}
