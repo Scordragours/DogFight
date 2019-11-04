485289bee74133f856fc03f43637755fbc2c6963
Scordragours <arthur.lecras@viacesi.fr>
2019-11-04 13:28:22 +0100

    - Add UpdateMap in Main.

--- main/src/main/java/main/Main.java
+++ main/src/main/java/main/Main.java
@@ -6,7 +6,10 @@ public class Main{
     public static void main(String args[]){
         Model Model = new Model(25, 25);
         Timer Timer = new Timer(Model);
+        UpdateMap UpdateMap = new UpdateMap(Model);
+        UpdateMap.start();
         Model.setTimer(Timer);
         Model.StartServer();
+
     }
 }
